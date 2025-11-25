c410a8e0af56e200d3ebc87a2884c0ce266bf923
webplotcentersj-hash <web.plotcentersj@gmail.com>
2025-11-25 16:42:51 -0300

    Add CORS headers to API endpoints

--- api/login.js
+++ api/login.js
@@ -2,6 +2,14 @@ import { getDb } from './db.js'
 import { comparePassword, createToken } from './auth.js'
 
 export default async function handler(req, res) {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }

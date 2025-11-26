43050320220bca6801888bc46c896131e64164e0
webplotcentersj-hash <web.plotcentersj@gmail.com>
2025-11-26 08:13:14 -0300

    Revert "Add CORS headers to all remaining API endpoints"
    This reverts commit f28958df87ddbe176453fb88cd29fa7337fcedeb.

--- api/users.js
+++ api/users.js
@@ -2,15 +2,6 @@ import { getDb } from './db.js'
 import { requireAuth } from './auth.js'
 
 export default async function handler(req, res) {
-  // CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end()
-  }
-
   try {
     const user = await requireAuth(req)
     const db = getDb()
